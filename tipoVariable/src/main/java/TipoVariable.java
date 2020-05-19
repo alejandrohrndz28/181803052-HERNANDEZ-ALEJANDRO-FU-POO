99cf6c23e1966cb78ca3105452fee589ad2dba35
alejandro hernandez perez <alejandro.hernandez1152@uppuebla.edu.mx>
2020-05-19 15:53:58 -0500

    actualizacion de repositorio

---
+++ tipoVariable/src/main/java/TipoVariable.java
@@ -0,0 +1,28 @@
+
+public class TipoVariable {
+    public static void main (String args[]){
+    
+        
+        //fuertemente tipado
+        
+        int a=10, b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        float pi=3.14f;
+        double pie2=3.14;
+        
+        boolean bandera=true;
+        
+        char letra= 'a';
+        String palabra= "puede ser un mensaje largo";
+        System.out.println( palabra);
+        System.out.println(palabra+ "y esto es una prueba de ello");
+        System.out.println("este es el valor de pi "+pi);
+        System.out.println("este es el valor del circulo"+(pi+a));
+        
+}
+    
+}
